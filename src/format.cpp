ae1487a2fa0cdc8a1cf6b87072875db3f643b4cd
Your Name <you@example.com>
2019-10-28 07:55:24 +0000

    UpTime working and format ElapsedTime impleminted

--- src/format.cpp
+++ src/format.cpp
@@ -8,4 +8,30 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds[[maybe_unused]]) { 
+    
+    string timeString = "";
+    int minutes, hours;
+    minutes = seconds/60;
+    hours = minutes/60;
+
+    if(hours < 10){
+        timeString+= "0";
+    }
+
+    timeString += std::to_string(hours) + ":";
+
+    if(minutes%60 < 10){
+        timeString+= "0";
+    }
+
+    timeString += std::to_string(minutes%60) + ":";
+
+    if(seconds%60 < 10){
+        timeString+= "0";
+    }
+
+    timeString += std::to_string(seconds%60);
+
+    return timeString;
+}

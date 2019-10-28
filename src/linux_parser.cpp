ab3f9cb0ebcc02976bb0b14d81a79c2e7d43e0be
Your Name <you@example.com>
2019-10-28 07:11:00 +0000

    UpTime() progress

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -91,7 +91,19 @@ float LinuxParser::MemoryUtilization() {
 }
 
 // TODO: Read and return the system uptime
-long LinuxParser::UpTime() { return 0; }
+long LinuxParser::UpTime() { 
+  long time = -1;
+  string line;
+  
+  std::ifstream stream(kProcDirectory + kUptimeFilename);
+  if (stream.is_open()) {
+    std::getline(stream, line);
+  	std::istringstream linestream(line);
+    linestream >> time;
+  }
+  
+  return time;
+}
 
 // TODO: Read and return the number of jiffies for the system
 long LinuxParser::Jiffies() { return 0; }
